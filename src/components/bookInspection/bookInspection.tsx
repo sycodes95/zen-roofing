dade4d29d1904c4c6abfca41e2c98f790daa68f1
sycodes95 <sycodes95@gmail.com>
2023-11-18 03:37:46 -0800

    form working perfectly

--- src/components/bookInspection/bookInspection.tsx
+++ src/components/bookInspection/bookInspection.tsx
@@ -2,26 +2,45 @@
 
 import residentialIcon from '../../assets/images/residential.png'
 import commercialIcon from '../../assets/images/commercial.png'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Image from 'next/image'
 import { Input } from "@/components/ui/input"
 import DatePicker from './datePicker'
 import { format } from 'date-fns'
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
 
 enum RoofTypes {
   'Commercial' = 'Commercial',
   'Residential' = 'Residential',
   '' = ''
 }
+
+const workTypes = [
+  "Installation",
+  "Repair",
+  "Maintenance",
+  "Replacement",
+  "Restoration",
+  "Inspection",
+  "Waterproofing and Sealing",
+  "Gutter Work",
+  "Ventilation Solutions",
+  "Customization and Specialty Work"
+]
+
 export default function BookInspection () {
 
   const [selectedRoof, setSelectedRoof] = useState<RoofTypes>(RoofTypes.Residential)
 
-  const [inspectionData, setInspectionData] = useState({
-    name: '',
-    email: '',
-
-  })
+  const [selectedWorkType, setSelectedWorkType] = useState('')
 
   const [preferredDate, setPreferredDate] = useState<Date | undefined>(undefined)
 
@@ -29,6 +48,10 @@ export default function BookInspection () {
     if(!selectedRoof) setSelectedRoof(roofType)
     selectedRoof === roofType ? setSelectedRoof(RoofTypes['']) : setSelectedRoof(roofType)
   }
+
+  useEffect(()=> {
+    console.log(selectedWorkType);
+  },[selectedWorkType])
 
   return (
     <div className='w-full h-full bg-white p-12 flex flex-col gap-8'>
@@ -52,26 +75,40 @@ export default function BookInspection () {
         </div>
       </div>
       <div className='flex justify-between gap-4 '>
-        <form className="flex flex-col" 
+        <form className="grid grid-cols-4 w-full gap-4" 
         target="_blank" action={`https://formsubmit.co/d8e105c261b4429d4d9bc2eea8d16131`} method="POST">
           <input className="hidden" type="text" name="_honey"/>
           <input type="hidden" name="_captcha" value="false"/>
-          
-          <div className='grid grid-cols-4 gap-4'>
-            
-            <Input className='rounded-none outline-none' type='email' name='email' placeholder='Enter Email' required/>
-            <Input className='rounded-none outline-none' type='name' name='name' placeholder='Enter Name' required/>
-            <Input className='rounded-none outline-none' type='text' name='address' placeholder='Enter Address' required/>
-            <Input className='rounded-none outline-none' type='text' name='city' placeholder='Enter City' required/>
-            <Input className='rounded-none outline-none' type='number' name='zipcode' placeholder='Enter Zip Code' required/>
-            <Input className='rounded-none outline-none hidden' type='text' name='date' value={preferredDate && format(preferredDate, 'yyyy-MM-dd')}/> 
-            <DatePicker
-            preferredDate={preferredDate}
-            setPreferredDate={setPreferredDate}
-            />
-          </div>
 
-          <button className="z-50 flex items-center justify-center w-full h-8 mt-8 transition-colors bg-green-400 bg-opacity-50 rounded-sm hover:bg-opacity-25" type="submit">Submit</button>
+          <Input className='rounded-none outline-none' type='email' name='email' placeholder='Enter Email' required/>
+          <Input className='rounded-none outline-none' type='name' name='name' placeholder='Enter Name' required/>
+          <Input className='rounded-none outline-none' type='text' name='address' placeholder='Enter Address' required/>
+          <Input className='rounded-none outline-none' type='text' name='city' placeholder='Enter City' required/>
+          <Input className='rounded-none outline-none' type='number' name='zipcode' placeholder='Enter Zip Code' required/>
+          <Input className='rounded-none outline-none hidden' type='text' name='roof type' value={selectedRoof}/> 
+          <Input className='rounded-none outline-none hidden' type='text' name='work type' value={selectedWorkType}/> 
+          <Input className='rounded-none outline-none hidden' type='text' name='preferred date' value={preferredDate && format(preferredDate, 'yyyy-MM-dd')}/> 
+
+          <DatePicker
+          preferredDate={preferredDate}
+          setPreferredDate={setPreferredDate}
+          />
+
+          <Select onValueChange={(value) => setSelectedWorkType(value)}>
+            <SelectTrigger className="w-full h-full p-2 outline-none">
+              <SelectValue className='outline-none' placeholder="Work Type" />
+            </SelectTrigger>
+            <SelectContent className='w-full h-full flex flex-col gap-2'>
+              {
+              workTypes.map((work) => (
+                <SelectItem className='cursor-pointer hover:bg-zinc-400 transition-all' key={work} value={work} >{work}</SelectItem>
+              ))
+              }
+            </SelectContent>
+          </Select>
+          
+
+          <button className="z-50 flex items-center justify-center h-full w-full transition-colors bg-green-400 bg-opacity-50 rounded-sm hover:bg-opacity-25" type="submit">Submit</button>
         </form>
       </div>
     </div>
