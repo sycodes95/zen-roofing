ab0695119eb63bbd8dd6759bf61f00aca6acc0cb
sycodes95 <sycodes95@gmail.com>
2023-12-20 09:38:03 -0800

    simplified the process

--- src/app/landingPage/components/theProcess/processCard.tsx
+++ src/app/landingPage/components/theProcess/processCard.tsx
@@ -7,11 +7,11 @@ interface ProcessCardProps {
 
 export default function ProcessCard ({card, index} : ProcessCardProps) {
   return (
-    <div className='flex items-center gap-8 text-jet  bg-opacity-50 rounded-l-none rounded-xl h-full' key={index} >
-      <div className="p-2 h-full w-fit bg-white rounded-xl  rounded-l-none">
+    <div className='flex items-center gap-8 text-jet rounded-l-none rounded-xl h-full' key={index} >
+      <div className="p-2 h-full w-fit  rounded-xl border-2 border-black rounded-l-none">
         {card.icon}
       </div>
-      <div className="h-12 items-center w-full flex">
+      <div className="h-full p-4 items-center w-full border-2 flex bg-black rounded-l-xl bg-opacity-90">
         <span className=' font-inter-tight text-sm font-semibold text-left h-fit flex  text-white'>{card.description}</span>
 
       </div>
--- src/app/landingPage/components/theProcess/theProcess.tsx
+++ src/app/landingPage/components/theProcess/theProcess.tsx
@@ -22,13 +22,13 @@ export default function TheProcess () {
   return (
     <div className="relative flex justify-end gap-8 h-full w-full rounded-xl ">
 
-      <div className='relative md:absolute z-10 md:top-1/2 md:-translate-y-1/2 md:w-1/2 md:left-0 flex flex-col text-jet  h-fit md:p-0'>
-        <div className=' p-4 rounded-t-xl border-2 border-b-0 border-orange-500 flex flex-col gap-4 bg-white bg-opacity-50 '>
+      <div className='relative z-10 flex flex-col text-jet  h-fit md:p-0'>
+        <div className=' p-4 rounded-t-xl flex flex-col gap-4 bg-white bg-opacity-50 '>
           <span className='text-sm font-semibold font-inter-tight rounded-2xl bg-black bg-opacity-90 text-white  w-fit pl-4 pr-4'># The Process</span>
           <span className='font-display-2  text-5xl pl-2 rounded-xl text-black text-opacity-90 mt-2'>Simple steps to becoming a happy customer in no time!</span>
         </div>
         
-        <div className="flex flex-col gap-4 h-full w-full rounded-b-xl border border-t-0 bg-black border-black border-opacity-90 bg-opacity-90 p-4">
+        <div className="flex flex-col gap-4 h-full w-full rounded-b-xl bg-white bg-opacity-50 p-4">
           {
           process.map((card, index) => (
             <ProcessCard 
@@ -45,8 +45,8 @@ export default function TheProcess () {
         <Image className='md:hidden opacity-30 object-cover grayscale absolute top-0 left-0 h-full w-full rounded-2xl' src={roofDrill} alt="" />
       </div>
 
-      <div className='hidden md:flex justify-end right-0 relative h-[600px] min-w-max w-3/4  rounded-xl overflow-hidden '>
-        <Image className='opacity-100  object-cover  absolute top-3/4 -translate-y-3/4 w-full h-full grayscale rounded-2xl' src={roofDrill} alt="" priority/>
+      <div className='hidden md:flex justify-end  relative max-h-[100%] min-w-max w-full  rounded-xl overflow-hidden '>
+        <Image className='opacity-100 absolute top-0 right-0 object-cover w-full h-full grayscale rounded-2xl' src={roofDrill} alt="" priority/>
         <div className='top-0  absolute h-full w-1/3 bg-orange-500 bg-opacity-50'>
           
         </div>
